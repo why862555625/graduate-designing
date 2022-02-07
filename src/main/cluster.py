43fded44ddb1ba0a991cbb94d64e21d1988a0d2e
王海运 <862555625@qq.com>
2022-02-07 15:26:38 +0800

    init paper

--- src/main/cluster.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.cluster import KMeans
-from src.main.cluster_init_drawging import init_cluster_data
-from sklearn.metrics import fowlkes_mallows_score
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-np.random.seed(5)
-x, y = init_cluster_data()
